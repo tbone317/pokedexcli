260259732edfec2bb7115153b1dcbfd1ec075a46
Travis Dunbar <travis.j.dunbar@gmail.com>
2026-02-23 15:47:15 -0600

    explore area support

--- command_help.go
+++ command_help.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func commandHelp(cfg *config) error {
-       fmt.Println("Welcome to the Pokedex!")
-       fmt.Println("Usage:")
-       fmt.Println("")
-       for _, cmd := range getCommands() {
-	       fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-       }
-       return nil
+func commandHelp(cfg *config, args ...string) error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
 }
